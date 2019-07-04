85be5c93db55122353edc2c15ba94c09d3275d34
Mateus Oliveira Costa Bezerra <mateus.oliveira.residente@tre-rn.jus.br>
2019-07-04 15:22:59 -0300

    restruturando o código

--- web_scraper.py
+++ web_scraper.py
@@ -3,20 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 
 
-page = requests.get('https://rn.olx.com.br/imoveis')
+def get_pagina_itens():
+    page = requests.get('https://rn.olx.com.br/imoveis')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    list_itens = soup.find(class_='section_OLXad-list')
+    itens = list_itens.find_all(class_='item')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-
-pag2 = 'https://rn.olx.com.br/imoveis?o=2'
-pag3 = 'https://rn.olx.com.br/imoveis?o=3'
-
-list_itens = soup.find(class_= 'section_OLXad-list')
-
-itens = list_itens.find_all(class_='item')
-prices = itens.find(class_='OLXad-list-price')
-
-for x in itens:
-  print(x.prettify())
+    return itens
 
 
+def get_links(itens):
+    links = []
+    for x in itens:
+      link = x.find(class_='OLXad-list-link')
+      if link != None:
+        links.append(link['href'])
+    
+    return links
 
+def get_dados_link(link):
+  page = requests.get(link)
+  soup = BeautifulSoup(page.text, 'html.parser')
+  list_loc = soup.find(class_='OLXad-location')
+  for loc in list_loc.find_all(class_='description'):
+    print(loc)
+  
+if __name__ == '__main__':
+    itens = get_pagina_itens()
+    links = get_links(itens)
+    for link in links:
+      get_dados_link(link)
